7456be1c3a48520b894454c175da72870150e4e8
Ajee <93165947+ajee10x@users.noreply.github.com>
2023-01-12 13:04:04 -0500

    Create main.py

---
+++ tinder-data-analysis-bot/main.py
@@ -0,0 +1,60 @@
+import requests
+import json
+
+# Authenticate with the Tinder API
+def authenticate(facebook_token, facebook_id):
+    url = 'https://api.gotinder.com/v2/auth/login/facebook'
+    headers = {'User-agent': 'Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)'}
+    payload = {'facebook_token': facebook_token, 'facebook_id': facebook_id}
+    r = requests.post(url, headers=headers, json=payload)
+    if r.status_code != 200:
+        raise Exception('Failed to authenticate: {}'.format(r.text))
+    return r.json()
+
+# Get the user's profile data
+def get_profile(access_token):
+    url = 'https://api.gotinder.com/v2/profile'
+    headers = {'User-agent': 'Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)',
+               'X-Auth-Token': access_token}
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        raise Exception('Failed to get profile: {}'.format(r.text))
+    return r.json()
+
+# Get the user's recommendations
+def get_recommendations(access_token):
+    url = 'https://api.gotinder.com/v2/recs/core'
+    headers = {'User-agent': 'Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)',
+               'X-Auth-Token': access_token}
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        raise Exception('Failed to get recommendations: {}'.format(r.text))
+    return r.json()
+
+# Main function to run the data analysis bot
+def main():
+    facebook_token = 'your_facebook_token_goes_here'
+    facebook_id = 'your_facebook_id_goes_here'
+    auth_response = authenticate(facebook_token, facebook_id)
+    access_token = auth_response['data']['api_token']
+    profile_data = get_profile(access_token)
+    recommendations_data = get_recommendations(access_token)
+
+    # Perform data analysis on the profile and recommendations data
+    # ...
+
+if __name__ == '__main__':
+    main()
+
+################################
+#         Bot Details
+# TinderBots/tinder-data-analysis-bot/main.py 
+# Version: 1.0
+################################
+#         Copy Right Details
+# © OpenBotBook
+# https://github.com/OpenBotBook
+# Apache License
+# Version 2.0, January 2004
+# http://www.apache.org/licenses/
+################################
